c73279f3c5f89226a477c22faa2cc5274d90a111
musung <musungkim1352@gmail.com>
2023-09-04 18:24:38 +0900

    a

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  chainWebpack: (config) => {
-    // 환경 변수 값을 포함시킬 수 있도록 DefinePlugin을 사용
-    config.plugin('define').tap((definitions) => {
-      definitions[0]['process.env'].VUE_APP_API_KEY = JSON.stringify(process.env.VUE_APP_API_KEY);
-      return definitions;
-    });
-  },
+  transpileDependencies: true
 })
-// vue.config.js
-
